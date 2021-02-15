5e1c3fdd4d5c027816f50dd9244b56c89a3e7bd7
Umebosh1 <79074515+Umebosh1@users.noreply.github.com>
2021-02-15 17:34:04 +0900

    Add files via upload

---
+++ challenge1.py
@@ -0,0 +1,5 @@
+import statistics
+list=[12,45,23,90,76,46]
+a=statistics.median_low(list)
+b=statistics.median_high(list)
+print("a:{},b:{}".format(a,b))
---
+++ cubed.py
@@ -0,0 +1,8 @@
+import math
+
+def cube():
+    a=input("3乗する数を入れてください")
+    a=int(a)
+    a=math.pow(a,3)
+    print(a)
+
